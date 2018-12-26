2a72e48fd10c58e12eb19d8e3fa34a77e0b3fe9a
amnuay9957 <46090160+amnuay9957@users.noreply.github.com>
2018-12-26 15:27:25 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -17,3 +17,32 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 echo $result;
+
+
+<?php
+    $access_token = 'tCJl30erl0I50K93pJ70Q0u7kLGAqHhF8vyGU80jhDPlqVfkRt8yTZyswqvUfLNpqBCd5taYXo/mhPnH8GV3Fh7O+ctXbtsCxTYs2qW/SYNOwPpzX47seaZryAlxSckWmLcHPbUO5Ub3UeLci9++HgdB04t89/1O/w1cDnyilFU=';//copy Channel access token ตอนที่ตั้งค่ามาใส่
+
+    $content = file_get_contents('php://input');
+    $arrayJson = json_decode($content, true);
+
+    $arrayHeader = array();
+    $arrayHeader[] = "Content-Type: application/json";
+    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+
+    //รับข้อความจากผู้ใช้
+    $message = $arrayJson['events'][0]['message']['text'];
+#ตัวอย่าง Message Type "Text"
+    if($message == "สวัสดี"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+        replyMsg($arrayHeader,$arrayPostData);
+    }
+    #ตัวอย่าง Message Type "Sticker"
+    else if($message == "ฝันดี"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "sticker";
+        $arrayPostData['messages'][0]['packageId'] = "2";
+        $arrayPostData['messages'][0]['stickerId'] = "46";
+        replyMsg($arrayHeader,$arrayPostData);
+    }
